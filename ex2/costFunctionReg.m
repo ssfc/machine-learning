eb46a6ca8c2fd93f2c2295af6c05ed1e9c2664ea
YE GAO <sortsimilar@gmail.com>
2019-03-27 19:34:37 +0800

    pass

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,8 +17,37 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+sigma1 = 0;
+sigma2 = 0;
+sigma3 = 0;
+hypothesis = zeros(m, 1);
+
+for i = 1:m
+    hypothesis(i) = sigmoid(dot(theta, X(i,:)));
+endfor
+
+for i = 1:m
+    each = -y(i)*log(hypothesis(i)) - (1-y(i))*log(1-hypothesis(i));
+    sigma1 = sigma1 + each;
+endfor
+
+for i = 2:size(theta)
+    sigma2 = sigma2 + (theta(i))^2;
+endfor
+
+J = (1/m)*sigma1 + (lambda/(2*m))*sigma2;
+
+% compute the partial derivatives and set grad to the partial ===
+% derivatives of the cost w.r.t. each parameter in theta
+
+grad(1) = (1/m) * dot(hypothesis-y, X(1:end, 1));
 
 
+% grad(2) = (1/m) * dot(hypothesis-y, X(1:end, 2)) + (lambda / m)*theta(2);
+
+for i = 2:size(theta)
+    grad(i) = (1/m) * dot(hypothesis-y, X(1:end, i)) + (lambda / m)*theta(i);
+endfor
 
 
 
