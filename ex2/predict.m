7de5c02c5d5563c05ff7fed44c3b712dd075db53
YE GAO <sortsimilar@gmail.com>
2019-03-27 11:28:07 +0800

    predict

--- ex2/predict.m
+++ ex2/predict.m
@@ -8,6 +8,7 @@ m = size(X, 1); % Number of training examples
 
 % You need to return the following variables correctly
 p = zeros(m, 1);
+probability = zeros(m, 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
@@ -15,11 +16,17 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+%prob = sigmoid([1 45 85] * theta);
 
-
-
-
-
+for i=1:m
+    probability(i) = sigmoid(X(i,1:end) * theta);
+    
+    if(probability(i) >= 0.5)
+        p(i) = 1;
+    else
+        p(i) = 0;
+    endif
+endfor
 
 % =========================================================================
 
