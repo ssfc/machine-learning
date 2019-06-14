b687210879829e2713197b71b5b80ca912aa177c
YE GAO <sortsimilar@gmail.com>
2019-06-14 20:21:48 +0800

    finish

--- ex1_py/computeCost.py
+++ ex1_py/computeCost.py
@@ -1,3 +1,7 @@
+#    COMPUTECOST Compute cost for linear regression
+#    J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+#    parameter for linear regression to fit the data points in X and y
+
 import numpy as np
 
 def computeCost(X, y, theta) :
